919f94e1169751f6978e2ff3d4b6cbbdbb3534a5
Ritika-Lath <ritikalatth@gmail.com>
2024-10-18 23:34:08 +0530

    added function and test case for single input

--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -4,5 +4,9 @@ describe('String Calculator',()=>{
 
     test('should return 0 for an empty string', () => {
         expect(addString("")).toBe(0);
-      });
+    });
+
+    test('should return same number for a single string', () => {
+        expect(addString("6")).toBe(6);
+    });
 })
--- src/utils/addString.js
+++ src/utils/addString.js
@@ -1,5 +1,7 @@
-export const addString=(number)=>{
-    if(!number){
+export const addString=(numbers)=>{
+    if(!numbers){
         return 0
     }
+    const num = parseInt(numbers, 10);
+  return isNaN(num)?0:num
 }
