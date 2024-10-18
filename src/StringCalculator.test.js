60a048606e6b1a92e96e3f84f59b9bf93c659e76
Ritika-Lath <ritikalatth@gmail.com>
2024-10-18 23:37:09 +0530

    added test and function to handle multiple numbers

--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -6,7 +6,15 @@ describe('String Calculator',()=>{
         expect(addString("")).toBe(0);
     });
 
-    test('should return same number for a single string', () => {
+    test('should return same number for a single string if they are a number', () => {
         expect(addString("6")).toBe(6);
+    });
+
+    test('should return sum of numbers for multiple string if they are a number', () => {
+        expect(addString("1,5")).toBe(6);
+    });
+
+    test('should return sum of numbers for multiple string if they are a number', () => {
+        expect(addString("1,2,3,4")).toBe(10);
     });
 })
--- src/utils/addString.js
+++ src/utils/addString.js
@@ -2,6 +2,8 @@ export const addString=(numbers)=>{
     if(!numbers){
         return 0
     }
-    const num = parseInt(numbers, 10);
-  return isNaN(num)?0:num
+    const numArray = numbers.split(',').map(num => parseInt(num, 10));
+  
+  // Step 3: Handle multiple numbers
+  return numArray.reduce((sum, num) => sum + (isNaN(num)?0:num),0)
 }
